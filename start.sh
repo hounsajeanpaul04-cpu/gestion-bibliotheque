3ca1477661303d640d4e0cab9e3203955714f7ff
Jean Paul HOUNSA <hounsajeanpaul04@gmail.com>
2026-04-22 14:15:13 +0100

    Fix start.sh sans CRLF

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
+@"
 #!/bin/sh
 cp /etc/secrets/.env /var/www/html/.env
 php artisan config:clear
 php artisan migrate --force
 php artisan tinker --execute="\App\Models\User::firstOrCreate(['email' => 'admin@bibliotheque.com'], ['name' => 'Admin', 'password' => bcrypt('password123'), 'is_admin' => true]);"
 apache2-foreground
+"@ | Set-Content -NoNewline -Encoding UTF8 start.sh
