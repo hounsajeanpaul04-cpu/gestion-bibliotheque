51714e0ba8675d3ca0c86e6fb5a5ea43375f7bc3
Jean Paul HOUNSA <hounsajeanpaul04@gmail.com>
2026-04-22 23:07:56 +0100

    Fix: Apache listen on Render PORT

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 php artisan config:clear
 php artisan migrate --force
+sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-enabled/*.conf
 apache2-foreground
