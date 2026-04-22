49e885668609f35f6b7db1958e7ab03de4039207
Jean Paul HOUNSA <hounsajeanpaul04@gmail.com>
2026-04-23 00:50:59 +0100

    Fix: Apache port 10000 for Render

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-php artisan config:clear
-php artisan migrate --force
-sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
-sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-enabled/*.conf
-apache2-foreground
+$lines = @(
+    "#!/bin/sh",
+    "php artisan config:clear",
+    "php artisan migrate --force",
+    "sed -i 's/Listen 80/Listen 10000/' /etc/apache2/ports.conf",
+    "sed -i 's/<VirtualHost \*:80>/<VirtualHost \*:10000>/' /etc/apache2/sites-enabled/000-default.conf",
+    "apache2-foreground"
+)
+$content = $lines -join "`n"
+[System.IO.File]::WriteAllText("D:\bibliotheque\gestion\start.sh", $content, [System.Text.UTF8Encoding]::new($false))
