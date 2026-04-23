d740154051e3d65b56639d51534d9d75a83d6aaf
Jean Paul HOUNSA <hounsajeanpaul04@gmail.com>
2026-04-23 01:45:03 +0100

    Fix: session config for HTTPS

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 php artisan config:clear
+php artisan config:cache
 php artisan migrate --force
 sed -i 's/Listen 80/Listen 10000/' /etc/apache2/ports.conf
 sed -i 's/*:80>/*:10000>/' /etc/apache2/sites-enabled/000-default.conf
