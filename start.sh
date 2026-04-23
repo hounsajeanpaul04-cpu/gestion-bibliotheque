b54d7e748078bf4c8e15649736c22164d7ab8bea
Jean Paul HOUNSA <hounsajeanpaul04@gmail.com>
2026-04-23 02:36:43 +0100

    Fix: session table + trust proxies

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 php artisan config:clear
 php artisan config:cache
 php artisan migrate --force
+php artisan session:table 2>/dev/null || true
+php artisan migrate --force
 sed -i 's/Listen 80/Listen 10000/' /etc/apache2/ports.conf
 sed -i 's/*:80>/*:10000>/' /etc/apache2/sites-enabled/000-default.conf
 apache2-foreground
